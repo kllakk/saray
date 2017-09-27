f4d8bb331dd466540ff7cba0f9b34ef6f04013ed
kllakk <ma@ctoh.net>
2017-09-28 01:02:29 +0300

    ok

--- saray/Program.cs
+++ saray/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace saray
 {
@@ -7,9 +8,9 @@ namespace saray
     {
         static void Main(string[] args)
         {
-            List<int> A = new List<int>() { 1, 1, 1, 4, 4, 2, 3, 3, 2, 2 };
+            List<int> A = InitialA();
 
-            var result = Calculate(A);
+            var result = CalculateLR(A);
 
             Console.WriteLine(string.Format("L: [{0}]", string.Join(", ", result.L)));
             Console.WriteLine(string.Format("R: [{0}]", string.Join(", ", result.R)));
@@ -17,12 +18,68 @@ namespace saray
             Console.ReadLine();
         }
 
-        static (List<int> L, List<int> R) Calculate(List<int> A)
+        static List<int> InitialA()
         {
-            List<int> L = new List<int>();
-            List<int> R = new List<int>();
+            return new List<int>() { 10, 1, 1, 1, 4, 4, 2, 3, 3, 2, 2 };
+        }
+
+        static (List<int> L, List<int> R) CalculateLR(List<int> A)
+        {            
+            var data = A.Where((x, i) => i > 0).Select((x, i) => new Data { Value = x, Index = i }).ToList();
+
+            List<int> L = TakeOne(data, Type.ToLeft);
+            List<int> R = TakeOne(data, Type.ToRight);
 
             return (L, R);
+        }
+
+        static List<int> TakeOne(List<Data> data, Type type)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var d in data)
+            {
+                int? value = null;
+
+                List<Data> range = new List<Data>();
+
+                switch (type) {
+                    case Type.ToLeft:
+                        range.AddRange(data.Take(d.Index + 1).OrderByDescending(x => x.Index).ToList());
+                        break;
+                    case Type.ToRight:
+                        range.AddRange(data.Skip(d.Index).OrderBy(x => x.Index).ToList());
+                        break;
+                }
+
+                foreach (var r in range)
+                {
+                    if (r.Value >= d.Value)
+                    {
+                        value = r.Index;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                result.Add((int)value);
+            }
+
+            return result;
+        }
+
+        enum Type
+        {
+            ToLeft,
+            ToRight
+        }
+
+        class Data
+        {
+            public int Index { get; set; }
+            public int Value { get; set; }
         }
     }
 }
