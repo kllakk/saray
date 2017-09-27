f033d64a496ab8fabc6717f2f6b299c488b96699
kllakk <ma@ctoh.net>
2017-09-27 23:25:41 +0300

    init

--- saray/Program.cs
+++ saray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace saray
 {
@@ -6,7 +7,22 @@ namespace saray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> A = new List<int>() { 1, 1, 1, 4, 4, 2, 3, 3, 2, 2 };
+
+            var result = Calculate(A);
+
+            Console.WriteLine(string.Format("L: [{0}]", string.Join(", ", result.L)));
+            Console.WriteLine(string.Format("R: [{0}]", string.Join(", ", result.R)));
+
+            Console.ReadLine();
+        }
+
+        static (List<int> L, List<int> R) Calculate(List<int> A)
+        {
+            List<int> L = new List<int>();
+            List<int> R = new List<int>();
+
+            return (L, R);
         }
     }
 }
